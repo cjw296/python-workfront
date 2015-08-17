754f9bcbcb5a3eb6e388637060c491bc40ed5d88
Chris Withers <chris@withers.org>
2015-08-17 14:28:07 +0100

    initial codegen from workfront metadata

---
+++ workfront/generated_objects.py
@@ -0,0 +1,754 @@
+# generated from https://api-cl01.attask-ondemand.com/attask/api/unsupported
+from .meta import Object
+
+
+class AccessLevel(Object):
+    code = "ACSLVL"
+
+
+class AccessLevelPermissions(Object):
+    code = "ALVPER"
+
+
+class AccessRequest(Object):
+    code = "ACSREQ"
+
+
+class AccessRule(Object):
+    code = "ACSRUL"
+
+
+class AccessRulePreference(Object):
+    code = "ARPREF"
+
+
+class AccessScope(Object):
+    code = "ACSCP"
+
+
+class AccessScopeAction(Object):
+    code = "ASCPAT"
+
+
+class AccessToken(Object):
+    code = "ACSTOK"
+
+
+class AccountRep(Object):
+    code = "ACNTRP"
+
+
+class Acknowledgement(Object):
+    code = "ACK"
+
+
+class Announcement(Object):
+    code = "ANCMNT"
+
+
+class AnnouncementAttachment(Object):
+    code = "ANMATT"
+
+
+class AnnouncementOptOut(Object):
+    code = "AMNTO"
+
+
+class AppBuild(Object):
+    code = "APPBLD"
+
+
+class AppEvent(Object):
+    code = "APEVT"
+
+
+class AppGlobal(Object):
+    code = "APGLOB"
+
+
+class AppInfo(Object):
+    code = "APPINF"
+
+
+class Approval(Object):
+    code = "APPROVAL"
+
+
+class ApprovalPath(Object):
+    code = "ARVPTH"
+
+
+class ApprovalProcess(Object):
+    code = "ARVPRC"
+
+
+class ApprovalProcessAttachable(Object):
+    code = "APRPROCATCH"
+
+
+class ApprovalStep(Object):
+    code = "ARVSTP"
+
+
+class ApproverStatus(Object):
+    code = "ARVSTS"
+
+
+class Assignment(Object):
+    code = "ASSGN"
+
+
+class AuditLoginAsSession(Object):
+    code = "AUDS"
+
+
+class Authentication(Object):
+    code = "AUTH"
+
+
+class Avatar(Object):
+    code = "AVATAR"
+
+
+class AwaitingApproval(Object):
+    code = "AWAPVL"
+
+
+class BackgroundJob(Object):
+    code = "BKGJOB"
+
+
+class Baseline(Object):
+    code = "BLIN"
+
+
+class BaselineTask(Object):
+    code = "BSTSK"
+
+
+class BillingRecord(Object):
+    code = "BILL"
+
+
+class Branding(Object):
+    code = "BRND"
+
+
+class CalendarEvent(Object):
+    code = "CALEVT"
+
+
+class CalendarFeedEntry(Object):
+    code = "CALITM"
+
+
+class CalendarInfo(Object):
+    code = "CALEND"
+
+
+class CalendarPortalSection(Object):
+    code = "CALPTL"
+
+
+class CalendarSection(Object):
+    code = "CALSEC"
+
+
+class CallableExpression(Object):
+    code = "CALEXP"
+
+
+class Category(Object):
+    code = "CTGY"
+
+
+class CategoryCascadeRule(Object):
+    code = "CTCSRL"
+
+
+class CategoryCascadeRuleMatch(Object):
+    code = "CTCSRM"
+
+
+class CategoryParameter(Object):
+    code = "CTGYPA"
+
+
+class CategoryParameterExpression(Object):
+    code = "CTGPEX"
+
+
+class Company(Object):
+    code = "CMPY"
+
+
+class ComponentKey(Object):
+    code = "CMPSRV"
+
+
+class CustomEnum(Object):
+    code = "CSTEM"
+
+
+class CustomEnumOrder(Object):
+    code = "CSTEMO"
+
+
+class CustomMenu(Object):
+    code = "CSTMNU"
+
+
+class CustomMenuCustomMenu(Object):
+    code = "CMSCMS"
+
+
+class CustomQuarter(Object):
+    code = "CSTQRT"
+
+
+class Customer(Object):
+    code = "CUST"
+
+
+class CustomerFeedback(Object):
+    code = "CSFD"
+
+
+class CustomerPreferences(Object):
+    code = "CUSTPR"
+
+
+class CustsSections(Object):
+    code = "CSTSEC"
+
+
+class DocsFolders(Object):
+    code = "DOCFLD"
+
+
+class Document(Object):
+    code = "DOCU"
+
+
+class DocumentApproval(Object):
+    code = "DOCAPL"
+
+
+class DocumentFolder(Object):
+    code = "DOCFDR"
+
+
+class DocumentProvider(Object):
+    code = "DOCPRO"
+
+
+class DocumentProviderConfig(Object):
+    code = "DOCCFG"
+
+
+class DocumentProviderMetadata(Object):
+    code = "DOCMET"
+
+
+class DocumentRequest(Object):
+    code = "DOCREQ"
+
+
+class DocumentShare(Object):
+    code = "DOCSHR"
+
+
+class DocumentVersion(Object):
+    code = "DOCV"
+
+
+class Email(Object):
+    code = "EMAILC"
+
+
+class EmailTemplate(Object):
+    code = "EMLTPL"
+
+
+class Endorsement(Object):
+    code = "ENDR"
+
+
+class EndorsementShare(Object):
+    code = "ENDSHR"
+
+
+class EventHandler(Object):
+    code = "EVNTH"
+
+
+class EventSubscription(Object):
+    code = "EVTSUB"
+
+
+class ExchangeRate(Object):
+    code = "EXRATE"
+
+
+class Expense(Object):
+    code = "EXPNS"
+
+
+class ExpenseType(Object):
+    code = "EXPTYP"
+
+
+class ExternalDocument(Object):
+    code = "EXTDOC"
+
+
+class ExternalSection(Object):
+    code = "EXTSEC"
+
+
+class Favorite(Object):
+    code = "FVRITE"
+
+
+class Feature(Object):
+    code = "FEATR"
+
+
+class FinancialData(Object):
+    code = "FINDAT"
+
+
+class Group(Object):
+    code = "GROUP"
+
+
+class Hour(Object):
+    code = "HOUR"
+
+
+class HourType(Object):
+    code = "HOURT"
+
+
+class ImportRow(Object):
+    code = "IROW"
+
+
+class ImportTemplate(Object):
+    code = "ITMPL"
+
+
+class InstalledDDItem(Object):
+    code = "IDDI"
+
+
+class Issue(Object):
+    code = "OPTASK"
+
+
+class Iteration(Object):
+    code = "ITRN"
+
+
+class JournalEntry(Object):
+    code = "JRNLE"
+
+
+class JournalField(Object):
+    code = "JRNLF"
+
+
+class KickStart(Object):
+    code = "KSS"
+
+
+class LayoutTemplate(Object):
+    code = "LYTMPL"
+
+
+class LayoutTemplateCard(Object):
+    code = "LTMPLC"
+
+
+class LayoutTemplatePage(Object):
+    code = "LTMPLP"
+
+
+class LicenseOrder(Object):
+    code = "LICEOR"
+
+
+class Like(Object):
+    code = "LIKE"
+
+
+class LinkedFolder(Object):
+    code = "LNKFDR"
+
+
+class MasterTask(Object):
+    code = "MTSK"
+
+
+class MatchResult(Object):
+    code = "MATCHR"
+
+
+class MessageArg(Object):
+    code = "MSGARG"
+
+
+class MetaRecord(Object):
+    code = "PRSTOBJ"
+
+
+class Milestone(Object):
+    code = "MILE"
+
+
+class MilestonePath(Object):
+    code = "MPATH"
+
+
+class MobileDevice(Object):
+    code = "MOBILDVC"
+
+
+class NonWorkDay(Object):
+    code = "NONWKD"
+
+
+class Note(Object):
+    code = "NOTE"
+
+
+class NoteTag(Object):
+    code = "NTAG"
+
+
+class NotificationRecord(Object):
+    code = "TMNR"
+
+
+class ObjectCategory(Object):
+    code = "OBJCAT"
+
+
+class Parameter(Object):
+    code = "PARAM"
+
+
+class ParameterGroup(Object):
+    code = "PGRP"
+
+
+class ParameterOption(Object):
+    code = "POPT"
+
+
+class ParameterValue(Object):
+    code = "PVAL"
+
+
+class PopAccount(Object):
+    code = "POPA"
+
+
+class PortalProfile(Object):
+    code = "PTLPFL"
+
+
+class PortalSection(Object):
+    code = "PTLSEC"
+
+
+class PortalTab(Object):
+    code = "PTLTAB"
+
+
+class PortalTabSection(Object):
+    code = "PRTBSC"
+
+
+class Portfolio(Object):
+    code = "PORT"
+
+
+class Predecessor(Object):
+    code = "PRED"
+
+
+class Preference(Object):
+    code = "PROSET"
+
+
+class Program(Object):
+    code = "PRGM"
+
+
+class Project(Object):
+    code = "PROJ"
+
+
+class ProjectUser(Object):
+    code = "PRTU"
+
+
+class ProjectUserPrefs(Object):
+    code = "PRUPS"
+
+
+class ProjectUserRole(Object):
+    code = "PTEAM"
+
+
+class QueueDef(Object):
+    code = "QUED"
+
+
+class QueueTopic(Object):
+    code = "QUET"
+
+
+class QueueTopicGroup(Object):
+    code = "QUETGP"
+
+
+class Rate(Object):
+    code = "RATE"
+
+
+class Recent(Object):
+    code = "RECENT"
+
+
+class RecentMenuItem(Object):
+    code = "RECENTMENUITEM"
+
+
+class RecentUpdate(Object):
+    code = "RUPDTE"
+
+
+class RecurrenceRule(Object):
+    code = "RECR"
+
+
+class ReportFolder(Object):
+    code = "RPTFDR"
+
+
+class Reseller(Object):
+    code = "RSELR"
+
+
+class ReservedTime(Object):
+    code = "RESVT"
+
+
+class ResourceAllocation(Object):
+    code = "RSALLO"
+
+
+class ResourcePool(Object):
+    code = "RSPOOL"
+
+
+class Risk(Object):
+    code = "RISK"
+
+
+class RiskType(Object):
+    code = "RSKTYP"
+
+
+class Role(Object):
+    code = "ROLE"
+
+
+class RoutingRule(Object):
+    code = "RRUL"
+
+
+class SSOMapping(Object):
+    code = "SSOMAP"
+
+
+class SSOMappingRule(Object):
+    code = "SSOMR"
+
+
+class SSOOption(Object):
+    code = "SSOPT"
+
+
+class SSOUsername(Object):
+    code = "SSOUSR"
+
+
+class SandboxMigration(Object):
+    code = "SNDMG"
+
+
+class Schedule(Object):
+    code = "SCHED"
+
+
+class ScheduledReport(Object):
+    code = "SCHREP"
+
+
+class ScoreCard(Object):
+    code = "SCORE"
+
+
+class ScoreCardAnswer(Object):
+    code = "SCANS"
+
+
+class ScoreCardOption(Object):
+    code = "SCOPT"
+
+
+class ScoreCardQuestion(Object):
+    code = "SCOREQ"
+
+
+class SearchEvent(Object):
+    code = "SRCEVT"
+
+
+class SecurityAncestor(Object):
+    code = "SECANC"
+
+
+class Sequence(Object):
+    code = "SEQ"
+
+
+class SharingSettings(Object):
+    code = "SHRSET"
+
+
+class StepApprover(Object):
+    code = "SPAPVR"
+
+
+class Task(Object):
+    code = "TASK"
+
+
+class Team(Object):
+    code = "TEAMOB"
+
+
+class TeamMember(Object):
+    code = "TEAMMB"
+
+
+class TeamMemberRole(Object):
+    code = "TEAMMR"
+
+
+class Template(Object):
+    code = "TMPL"
+
+
+class TemplateAssignment(Object):
+    code = "TASSGN"
+
+
+class TemplatePredecessor(Object):
+    code = "TPRED"
+
+
+class TemplateTask(Object):
+    code = "TTSK"
+
+
+class TemplateUser(Object):
+    code = "TMTU"
+
+
+class TemplateUserRole(Object):
+    code = "TTEAM"
+
+
+class TimedNotification(Object):
+    code = "TMNOT"
+
+
+class Timesheet(Object):
+    code = "TSHET"
+
+
+class TimesheetProfile(Object):
+    code = "TSPRO"
+
+
+class TimesheetTemplate(Object):
+    code = "TSHTMP"
+
+
+class UIFilter(Object):
+    code = "UIFT"
+
+
+class UIGroupBy(Object):
+    code = "UIGB"
+
+
+class UIView(Object):
+    code = "UIVW"
+
+
+class Update(Object):
+    code = "UPDATE"
+
+
+class User(Object):
+    code = "USER"
+
+
+class UserActivity(Object):
+    code = "USERAC"
+
+
+class UserAvailability(Object):
+    code = "USRAVL"
+
+
+class UserGroups(Object):
+    code = "USRGPS"
+
+
+class UserNote(Object):
+    code = "USRNOT"
+
+
+class UserPrefValue(Object):
+    code = "USERPF"
+
+
+class UserResource(Object):
+    code = "USERRS"
+
+
+class UsersSections(Object):
+    code = "USRSEC"
+
+
+class WatchListEntry(Object):
+    code = "WATCH"
+
+
+class WhatsNew(Object):
+    code = "WTSN"
+
+
+class Work(Object):
+    code = "WORK"
+
+
+class WorkItem(Object):
+    code = "WRKITM"
