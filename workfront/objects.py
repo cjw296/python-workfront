dc39d2ee6436df8a7e75f90f8751ae05964862ac
Chris Withers <chris@withers.org>
2015-08-20 15:41:58 +0100

    first-pass at a convert_to_task implementation for issues.

--- workfront/objects.py
+++ workfront/objects.py
@@ -1,1 +1,18 @@
 from .generated_objects import *
+
+class Issue(Issue):
+
+    def convert_to_task(self):
+        data = self.session.put(
+            self.api_url()+'/convertToTask',
+            params=dict(
+                updates=dict(
+                    options=['preserveIssue',
+                             'preservePrimaryContact',
+                             'preserveUpdates'],
+                    task=dict(name=self.name,
+                              description=self.description,
+                              enteredByID=self.entered_by_id,
+                              )
+            )))
+        return Task(self.session, ID=data['result'])
