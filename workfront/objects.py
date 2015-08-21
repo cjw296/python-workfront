05d0d4c915e05dacf7a138561c2269e8bd572757
Chris Withers <chris@withers.org>
2015-08-21 14:14:44 +0100

    Implement methods to add comments on Issues, Tasks and Notes (ie: other comments).

--- workfront/objects.py
+++ workfront/objects.py
@@ -1,7 +1,18 @@
 from .generated_objects import *
 
 
-class Issue(Issue):
+class Commentable(object):
+
+    def add_comment(self, text):
+        comment = Note(self.session,
+                       note_text = text,
+                       note_obj_code = self.code,
+                       obj_id = self.id)
+        comment.save()
+        return comment
+
+
+class Issue(Commentable, Issue):
 
     def convert_to_task(self):
         data = self.session.put(
@@ -17,6 +28,23 @@ class Issue(Issue):
                               )
             )))
         return Task(self.session, ID=data['result'])
+
+
+class Note(Note):
+
+    def add_comment(self, text):
+        comment = Note(self.session,
+                       note_text = text,
+                       note_obj_code = self.note_obj_code,
+                       obj_id = self.obj_id,
+                       parent_note_id=self.id
+                       )
+        comment.save()
+        return comment
+
+
+class Task(Commentable, Task):
+    pass
 
 
 class Update(Update):
