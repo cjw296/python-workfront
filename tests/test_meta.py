eeb094e706fde35e09216f9d43120a572871717d
Chris Withers <chris@simplistix.co.uk>
2016-01-26 09:01:19 +0000

    re-shuffle of meta and tests for non-http bits.
    things are broken at this point, so doing on a branch until they're working again.

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
-from testfixtures import compare
+from testfixtures import compare, ShouldRaise
 
-from workfront.meta import APIVersion, Object
+from tests.test_session import MockOpenHelper
+from workfront.meta import APIVersion, Object, Field, FieldNotLoaded
 
 
 class TestAPIVersion(TestCase):
@@ -53,3 +54,114 @@ class TestAPIVersion(TestCase):
         result = api.from_data(None, dict(objCode='TEST'))
         self.assertTrue(isinstance(result, TestObject_))
         compare(result.something, expected='foo')
+
+
+class TestBaseObject(MockOpenHelper, TestCase):
+
+    def test_field_names(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+            field_two = Field('fieldTwo')
+        compare(TestObject.field_names, expected={'field_one', 'field_two'})
+
+    def test_field_names_of_subclass(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+        class TestObject_(TestObject):
+            field_two = Field('fieldTwo')
+        compare(TestObject_.field_names, expected={'field_one', 'field_two'})
+
+    def test_field_names_of_base_class(self):
+        compare(Object.field_names, expected=set())
+
+    def test_instantiate(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+            field_two = Field('fieldTwo')
+        session = object()
+        obj = TestObject(session, field_one='foo', fieldTwo='bar', ID='x')
+        compare(obj.session, session)
+        compare(obj.id, 'x')
+        compare(obj.field_one, 'foo')
+        compare(obj.field_two, 'bar')
+        compare(obj.fields.dirty(), {})
+        return obj
+
+    def test_instantiate_empty(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+        obj = TestObject()
+        compare(obj.session, None)
+        compare(obj.id, None)
+        with ShouldRaise(FieldNotLoaded('fieldOne')) as s:
+            obj.field_one
+        self.assertFalse(isinstance(s.raised, AttributeError))
+        with ShouldRaise(AttributeError):
+            obj.field_two
+        compare(obj.fields.dirty(), {})
+
+    def test_repr(self):
+        obj = self.test_instantiate()
+        compare(
+            repr(obj),
+            expected="<TestObject: "
+                     "{'fieldOne': 'foo', 'fieldTwo': 'bar', 'ID': 'x'}>"
+        )
+
+    def test_set_field(self):
+        obj = self.test_instantiate()
+        obj.field_one = 'boop'
+        compare(obj.fields,
+                expected={'fieldOne': 'boop', 'fieldTwo': 'bar', 'ID': 'x'})
+        compare(obj.fields.dirty(), expected={'fieldOne': 'boop'})
+
+    def test_set_id(self):
+        obj = self.test_instantiate()
+        with ShouldRaise(AttributeError):
+            obj.id = 'nope'
+
+    def test_field_from_class(self):
+        obj = self.test_instantiate()
+        class_ = obj.__class__
+        field = class_.field_one
+        self.assertTrue(isinstance(field, Field))
+        compare(field.workfront_name, expected='fieldOne')
+
+    def test_convert_name_field_instance(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+
+        compare(TestObject.convert_name(TestObject.field_one),
+                expected='fieldOne')
+
+    def test_convert_name_field_name(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+
+        compare(TestObject.convert_name('field_one'),
+                expected='fieldOne')
+
+    def test_convert_name_fallback(self):
+        class TestObject(Object): pass
+
+        compare(TestObject.convert_name('foo'),
+                expected='foo')
+
+    def test_field_spec(self):
+        class TestObject(Object):
+            field_one = Field('fieldOne')
+
+        compare(TestObject.field_spec('field_one', 'fieldTwo'),
+                expected='fieldOne,fieldTwo')
+
+    def test_api_url(self):
+        class TestObject(Object):
+            code = 'FOO'
+        compare(TestObject(ID='x').api_url(), expected='/FOO/x')
+
+    def test_api_url_no_id(self):
+        class TestObject(Object):
+            code = 'FOO'
+        with ShouldRaise(ValueError('TestObject has no ID')):
+            TestObject().api_url()
+
--- workfront/meta.py
+++ workfront/meta.py
@@ -20,113 +20,7 @@ class APIVersion(object):
         return self.by_code[data['objCode']](session, **data)
 
 
-class ObjectMeta(type):
-
-    def __new__(meta, name, bases, __dict__):
-        if name != 'Object':
-            field_names = set()
-            for key, obj in __dict__.items():
-                if isinstance(obj, Field):
-                    field_names.add(key)
-
-        cls = super(ObjectMeta, meta).__new__(meta, name, bases, __dict__)
-
-        if name != 'Object':
-            cls.registry[cls.code] = cls
-            cls.field_names = getattr(cls, 'field_names', set()) | field_names
-
-        return cls
-
-
-class ModificationTrackingDict(dict):
-
-    def __init__(self):
-        self.clean()
-
-    def clean(self, only=None):
-        if only:
-            for key in only:
-                self.dirty_keys.discard(key)
-        else:
-            self.dirty_keys = set()
-
-    def __setitem__(self, key, value):
-        super(ModificationTrackingDict, self).__setitem__(key, value)
-        self.dirty_keys.add(key)
-
-    def dirty(self):
-        data = {}
-        for key in self.dirty_keys:
-            data[key] = self[key]
-        return data
-
-
-class Object(object):
-
-    __metaclass__ = ObjectMeta
-    registry = {}
-
-    def __init__(self, session=None, **fields):
-        self.session = session
-        self.fields = ModificationTrackingDict()
-        for name, value in fields.items():
-            if name in self.field_names:
-                setattr(self, name, value)
-            else:
-                self.fields[name] = value
-        self.fields.clean()
-
-    def __repr__(self):
-        return '<{0}: {1}>'.format(self.__class__.__name__,
-                                   self.fields)
-
-    @property
-    def id(self):
-        return self.fields.get('ID')
-
-    @classmethod
-    def from_data(cls, session, data):
-        return cls.registry[data['objCode']](session, **data)
-
-    @classmethod
-    def convert_name(cls, field_name):
-        if isinstance(field_name, Field):
-            return field_name.workfront_name
-        field = getattr(cls, field_name, None)
-        if field is None:
-            return field_name
-        else:
-            return field.workfront_name
-
-    @classmethod
-    def field_spec(cls, *field_names):
-        workfront_fields = []
-        for field_name in field_names:
-            workfront_fields.append(cls.convert_name(field_name))
-        return ','.join(workfront_fields)
-
-    def api_url(self):
-        return '/{0}/{1}'.format(self.code, self.id)
-
-    def load(self, *field_names):
-        fields = self.session.get(self.api_url(),
-                                  dict(fields=self.field_spec(*field_names)))
-        self.fields.update(fields)
-        self.fields.clean(fields)
-
-    def save(self):
-        if self.id is None:
-            data = self.session.post('/'+self.code, self.fields)
-            self.fields.update(data)
-        else:
-            self.session.put(self.api_url(), self.fields.dirty())
-        self.fields.clean()
-
-    def delete(self):
-        self.session.delete(self.api_url())
-
-
-class FieldNotLoaded(AttributeError):
+class FieldNotLoaded(Exception):
     pass
 
 
@@ -171,3 +65,101 @@ class Collection(LoadingAttribute):
     def process(self, instance, data):
         return [Object.from_data(instance.session, object_data)
                 for object_data in data]
+
+
+class ModificationTrackingDict(dict):
+
+    def __init__(self):
+        self.clean()
+
+    def clean(self, only=None):
+        if only:
+            for key in only:
+                self.dirty_keys.discard(key)
+        else:
+            self.dirty_keys = set()
+
+    def __setitem__(self, key, value):
+        super(ModificationTrackingDict, self).__setitem__(key, value)
+        self.dirty_keys.add(key)
+
+    def dirty(self):
+        data = {}
+        for key in self.dirty_keys:
+            data[key] = self[key]
+        return data
+
+
+class ObjectMeta(type):
+
+    def __new__(meta, name, bases, __dict__):
+        field_names = set()
+        for key, obj in __dict__.items():
+            if isinstance(obj, Field):
+                field_names.add(key)
+        cls = super(ObjectMeta, meta).__new__(meta, name, bases, __dict__)
+        cls.field_names = getattr(cls, 'field_names', set()) | field_names
+        return cls
+
+
+class Object(object):
+
+    __metaclass__ = ObjectMeta
+    registry = {}
+
+    def __init__(self, session=None, **fields):
+        self.session = session
+        self.fields = ModificationTrackingDict()
+        for name, value in fields.items():
+            if name in self.field_names:
+                setattr(self, name, value)
+            else:
+                self.fields[name] = value
+        self.fields.clean()
+
+    def __repr__(self):
+        return '<{0}: {1}>'.format(self.__class__.__name__,
+                                   self.fields)
+
+    @property
+    def id(self):
+        return self.fields.get('ID')
+
+    @classmethod
+    def convert_name(cls, field_name):
+        if isinstance(field_name, Field):
+            return field_name.workfront_name
+        field = getattr(cls, field_name, None)
+        if field is None:
+            return field_name
+        else:
+            return field.workfront_name
+
+    @classmethod
+    def field_spec(cls, *field_names):
+        workfront_fields = []
+        for field_name in field_names:
+            workfront_fields.append(cls.convert_name(field_name))
+        return ','.join(workfront_fields)
+
+    def api_url(self):
+        if not self.id:
+            raise ValueError('{0} has no ID'.format(self.__class__.__name__))
+        return '/{0}/{1}'.format(self.code, self.id)
+
+    def load(self, *field_names):
+        fields = self.session.get(self.api_url(),
+                                  dict(fields=self.field_spec(*field_names)))
+        self.fields.update(fields)
+        self.fields.clean(fields)
+
+    def save(self):
+        if self.id is None:
+            data = self.session.post('/'+self.code, self.fields)
+            self.fields.update(data)
+        else:
+            self.session.put(self.api_url(), self.fields.dirty())
+        self.fields.clean()
+
+    def delete(self):
+        self.session.delete(self.api_url())
