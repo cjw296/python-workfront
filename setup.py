d534cc508636553e1ab97e3933a5f0de28dd85ad
Chris Withers <chris@simplistix.co.uk>
2016-02-10 08:37:57 +0000

    prepare for 0.3.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='workfront',
-    version='0.0.dev0',
+    version='0.3.0',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
