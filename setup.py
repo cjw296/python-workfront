de9ee7f56a1d48759c7bd94e90a8c2cf187674d2
Chris Withers <chris@simplistix.co.uk>
2016-02-11 07:47:30 +0000

    prepare for 0.6.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-workfront',
-    version='0.3.2',
+    version='0.6.0',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
@@ -15,9 +15,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.4',
-        # 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     packages=find_packages(),
     include_package_data=True,
